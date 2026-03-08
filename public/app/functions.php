f710248bb11d44516dc68d85adeb03b4ddaa8018
Salavat <webfuldeveloper@gmail.com>
2026-03-08 13:54:07 +0500

    обновление и перенос vendor

--- public/app/functions.php
+++ public/app/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '../../vendor/autoload.php';
 
 define('ELASTIC_INDEX', 'documents');
 define('CHUNK_SIZE', 800);
--- public/app/index.php
+++ public/app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '../../vendor/autoload.php';
 require_once 'functions.php';
 
 use GuzzleHttp\Client;
