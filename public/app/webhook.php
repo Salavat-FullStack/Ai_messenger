28eeab871cc5dc4bd18550aa1cca3101ff57ec17
Salavat <webfuldeveloper@gmail.com>
2026-04-07 19:46:25 +0500

    создание бота max

--- public/app/webhook.php
+++ public/app/webhook.php
@@ -1,2 +1,9 @@
 <?php
-echo "OK";
+// читаем входящий запрос
+$input = file_get_contents("php://input");
+
+// логируем (чтобы видеть что приходит)
+file_put_contents("log.txt", $input . PHP_EOL, FILE_APPEND);
+
+// ОБЯЗАТЕЛЬНО ответить 200
+http_response_code(200);
