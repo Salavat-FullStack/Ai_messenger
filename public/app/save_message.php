d3ee0af4f5ed3dcadd843d15c6d691f0270f3350
Salavat <webfuldeveloper@gmail.com>
2026-04-17 21:47:33 +0500

    разработка бота

--- public/app/save_message.php
+++ public/app/save_message.php
@@ -64,7 +64,7 @@ function saveMessage($client, $messageUser, $messageAi, $messageReview, $userDat
     $response = $client->index($params);
 
     if($response['_shards']['failed'] === 0){
-        return $response;
+        return $response['_id'];
     }else{
         return "Что-то пошло не так!";
     }
@@ -88,7 +88,7 @@ function saveMessageManager($client, $messageUser, $managerResponse, $userData, 
     $response = $client->index($params);
 
     if($response['_shards']['failed'] === 0){
-        return $response;
+        return $response['_id'];
     }else{
         return "Что-то пошло не так!";
     }
