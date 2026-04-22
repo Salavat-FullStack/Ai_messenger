c429ea14eab889efd79657652a1727ee98ae0f72
Salavat <webfuldeveloper@gmail.com>
2026-04-22 22:42:10 +0500

    интеграция

--- public/app/bot_max.php
+++ public/app/bot_max.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://localhost.akuprof.ru");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // $token = "f9LHodD0cOLwjuOGWlt5_5r_fk-nUKWe-e3hL5-f_Mg3JcT_L9d7gt1dqg8lbDBJJxPKQx6UEHnpoqND01Iy";
 
 // $url = "https://platform-api.max.ru/subscriptions";
--- public/app/index.php
+++ public/app/index.php
@@ -1,5 +1,16 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://localhost.akuprof.ru");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 require __DIR__ . '/../../vendor/autoload.php';
 require_once 'functions.php';
 
