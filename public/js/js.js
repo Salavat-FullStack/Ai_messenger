80569b78dbdc584335c557107f6072d2d050d0cd
Salavat <webfuldeveloper@gmail.com>
2026-04-22 22:35:31 +0500

    интеграция

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded',()=>{
     document.body.insertAdjacentHTML('beforeend', `
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    
     <div class="Ai_modal">
         <div class="Ai_nav">
             <div class="Ai_manager">
