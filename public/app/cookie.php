e92edd97a721bb7ea797b311819f6ec3650ebac3
Salavat <webfuldeveloper@gmail.com>
2026-04-23 12:55:04 +0500

    интеграция

--- public/app/cookie.php
+++ public/app/cookie.php
@@ -1,14 +1,20 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
 header("Access-Control-Allow-Origin: https://localhost.akuprof.ru");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
 
 define('COOKIE_GENERATE_KEY', $_ENV['COOKIE_GENERATE_KEY']);
 
