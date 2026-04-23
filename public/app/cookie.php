a2892d6cef03b8b2169dd6052e5c473a77e80600
Salavat <webfuldeveloper@gmail.com>
2026-04-23 13:05:31 +0500

    интеграция

--- public/app/cookie.php
+++ public/app/cookie.php
@@ -18,49 +18,45 @@ $dotenv->load();
 
 define('COOKIE_GENERATE_KEY', $_ENV['COOKIE_GENERATE_KEY']);
 
+$cookie_name = "ai_chat_cookie";
+
+function createUserCookie($secret, $cookie_name) {
+    // 1. создаём уникальный id
+    $id = bin2hex(random_bytes(16));
+
+    // 2. делаем подпись
+    $signature = hash_hmac('sha256', $id, $secret);
+
+    // 3. соединяем в одну строку
+    $value = $id . ':' . $signature;
+
+    // 4. сохраняем куку
+    setcookie($cookie_name, $value, [
+        'expires' => time() + (86400 * 30), // 30 дней
+        'path' => '/',
+        'httponly' => true,
+        'secure' => false, // true если HTTPS
+        'samesite' => 'Strict'
+    ]);
+
+    return $id;
+}
+
+if (!isset($_COOKIE[$cookie_name])) {
+    createUserCookie(COOKIE_GENERATE_KEY, $cookie_name);
+}
+
+list($userId, $token) = explode(":", $_COOKIE[$cookie_name]);
+
+$expected = hash_hmac('sha256', $userId, COOKIE_GENERATE_KEY);
+
+if (!hash_equals($expected, $token)) {
+    http_response_code(403);
+    exit("Invalid token");
+}
+
 echo json_encode([
-    "token" => COOKIE_GENERATE_KEY
+    "status" => "authorized",
+    "user_id" => $userId,
+    "token" => $token
 ]);
-
-// $cookie_name = "ai_chat_cookie";
-
-// function createUserCookie($secret, $cookie_name) {
-//     // 1. создаём уникальный id
-//     $id = bin2hex(random_bytes(16));
-
-//     // 2. делаем подпись
-//     $signature = hash_hmac('sha256', $id, $secret);
-
-//     // 3. соединяем в одну строку
-//     $value = $id . ':' . $signature;
-
-//     // 4. сохраняем куку
-//     setcookie($cookie_name, $value, [
-//         'expires' => time() + (86400 * 30), // 30 дней
-//         'path' => '/',
-//         'httponly' => true,
-//         'secure' => false, // true если HTTPS
-//         'samesite' => 'Strict'
-//     ]);
-
-//     return $id;
-// }
-
-// if (!isset($_COOKIE[$cookie_name])) {
-//     createUserCookie(COOKIE_GENERATE_KEY, $cookie_name);
-// }
-
-// list($userId, $token) = explode(":", $_COOKIE[COOKIE_GENERATE_KEY]);
-
-// $expected = hash_hmac('sha256', $userId, COOKIE_GENERATE_KEY);
-
-// if (!hash_equals($expected, $token)) {
-//     http_response_code(403);
-//     exit("Invalid token");
-// }
-
-// echo json_encode([
-//     "status" => "authorized",
-//     "user_id" => $userId,
-//     "token" => $token
-// ]);
