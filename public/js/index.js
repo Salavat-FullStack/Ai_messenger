ef375ebd83fdde06cded40521a146b3b6f6badd4
Salavat <webfuldeveloper@gmail.com>
2026-05-28 11:05:25 +0500

    доработка формы

--- public/js/index.js
+++ public/js/index.js
@@ -240,9 +240,9 @@ document.addEventListener('DOMContentLoaded',()=>{
 
             console.log(Ai_form.classList.contains('display_none'));
 
-            if(Ai_form.classList.contains('display_none')){
-                Ai_form.classList.remove('display_none');
-            }
+            // if(Ai_form.classList.contains('display_none')){
+            //     Ai_form.classList.remove('display_none');
+            // }
 
             if(Ai_form.classList.contains('display_none') === false){
                 const result = AiForm();
