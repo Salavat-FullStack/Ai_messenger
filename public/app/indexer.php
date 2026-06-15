7cff78bec4a8b451602782a580590c6c8b088f62
Salavat <webfuldeveloper@gmail.com>
2026-06-15 13:06:05 +0500

    созданиие новой базы

--- public/app/indexer.php
+++ public/app/indexer.php
@@ -2,7 +2,7 @@
 
 require_once "functions.php";
 
-define('DATA_DIR', __DIR__ . '/../dataText/parsed_products');
+define('DATA_DIR', __DIR__ . '/../dataText/parsed_products/about');
 define('MAX_CHAR_LIMIT', 4000); // Выносим лимит в константу для удобства
 
 $client = generatClient("client"); 
