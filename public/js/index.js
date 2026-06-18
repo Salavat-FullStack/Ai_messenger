193947e3e24f4f64cdd62f4cdcc8cad78fc437b6
Salavat <webfuldeveloper@gmail.com>
2026-06-18 17:35:18 +0500

    добовления запроса review

--- public/js/index.js
+++ public/js/index.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded',()=>{
                 request: question
             })
         })
-        .then(response => response.json())
+        .then(response => response.text())
         .then(data => {
             console.log(data);
             console.log(data['response']);
