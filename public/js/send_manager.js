3e8d2e8b3f782f1508f1bf69b3e80cace4d26400
Salavat <webfuldeveloper@gmail.com>
2026-07-26 14:21:25 +0500

    правки и добавление zvukoizol

--- public/js/send_manager.js
+++ public/js/send_manager.js
@@ -130,7 +130,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                         },
                         body: formDataMaxBot
                     })
-                    .then(response => response.json())
+                    .then(response => response.text())
                     .then(data => {
                         console.log(data);
                         console.log(data.status);
