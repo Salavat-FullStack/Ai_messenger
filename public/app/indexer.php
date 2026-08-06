1ccfe58fea36fc770a99d89971a5deee8d9c5be4
Salavat <webfuldeveloper@gmail.com>
2026-08-06 15:47:49 +0500

    добавление базы стройтовары

--- public/app/indexer.php
+++ public/app/indexer.php
@@ -2,7 +2,7 @@
 
 require_once "functions.php";
 
-define('DATA_DIR', __DIR__ . '/../dataText/documents_zvukoizol/about');
+define('DATA_DIR', __DIR__ . '/../dataText/stroytovary_products');
 define('MAX_CHAR_LIMIT', 4000); // Лимит на каждое текстовое поле по отдельности
 
 $client = generatClient("client"); 
@@ -51,7 +51,7 @@ foreach ($files as $file) {
     ];
 
     if (count($batch) === $batchSize) {
-        processAndSendWholeProducts($batch, $client, $es, "documents_zvukoizol");
+        processAndSendWholeProducts($batch, $client, $es, "documents_stroytovary");
         $batch = []; 
         usleep(500000); // Пауза 0.5 сек для лимитов OpenAI
     }
@@ -59,7 +59,7 @@ foreach ($files as $file) {
 
 
 if (count($batch) > 0) {
-    processAndSendWholeProducts($batch, $client, $es, "documents_zvukoizol");
+    processAndSendWholeProducts($batch, $client, $es, "documents_stroytovary");
 }
 
 echo "\nИндексация разделенных JSON товаров (с лимитом полей в " . MAX_CHAR_LIMIT . " симв.) успешно завершена!\n";
